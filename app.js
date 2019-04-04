8ac430b4277609d07e5fdfbd9b1a5eb301d1a476
ll9 <llwierer@web.de>
2019-04-04 15:11:40 +0200

    StyleDemo

--- app.js
+++ app.js
@@ -33,12 +33,41 @@ function generateMockFeatures(amount = 1) {
         ol.proj.fromLonLat([
           (LonCenter - 10 / 2) + Math.random() * 10,
           (LatCenter - 10 / 2) + Math.random() * 10
-        ])
+        ]),
       )
     );
+    feature.setProperties({
+        "Ort": "Prien",
+        "Montagedatum": "22.02.2019"
+    })
 
     source.addFeature(feature);
   }
 }
+
+/**
+ * 
+ * @param {ol.Feature} feature 
+ */
+function styleFunction(feature) {
+    let text = new ol.style.Text({
+        text: feature.getProperties()["Ort"],
+        stroke: new ol.style.Stroke({color: 'black', width: 4}),
+        fill: new ol.style.Fill({color: 'white'}),
+        font: '15px sans-serif'
+    });
+    let style = new ol.style.Style({
+        text: text,
+        image: new ol.style.Circle({
+            radius: 15,
+            stroke: new ol.style.Stroke({color: 'blue', width: 3}),
+            fill: new ol.style.Fill({color: 'rgba(0, 0, 255, 0.3)'})
+        })
+    })
+
+    return style;
+}
+
+layer.setStyle(styleFunction);
 
 generateMockFeatures(100);
